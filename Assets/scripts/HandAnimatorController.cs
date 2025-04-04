9aef1979a978e8200f9dd7d2869d0d7b0d8dd40a
unknown <salome.gobbi@gmail.com>
2025-04-04 14:49:52 +0200

    initial commit

---
+++ Assets/scripts/HandAnimatorController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandAnimatorController : MonoBehaviour
+{
+
+    [SerializeField] private InputActionProperty triggerAction;
+    [SerializeField] private InputActionProperty gripAction;
+    private Animator anim;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = triggerAction.action.ReadValue<float>();
+        float gripValue = gripAction.action.ReadValue<float>();
+
+        anim.SetFloat("Trigger", triggerValue);
+        anim.SetFloat("Grip", gripValue);
+    }
+}
